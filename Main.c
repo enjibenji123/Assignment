f5bf994aae25caa05875ba2bd2f43a4804b747f8
Ben Harris <enjibenji1234@gmail.com>
2019-04-28 12:44:43 +0000

    started on tasks

--- Main.c
+++ Main.c
@@ -6,10 +6,39 @@
 
 #include <stdio.h>
 
-int main() {
-  int a;
-  scanf("%d", &a);
-  printf("%d", a);
+int main() 
+{
+  int x;
+  char c;
+  scanf("%d\n", &x);
+  if (x == 1)
+  {
+  FILE *text = fopen("task1", "r"); 
+  while ( ( c = fgetc(text)) != EOF)
+    {
+    int xc = c;
+    printf("\n%d", xc);    
+    if ( 65 <= xc <= 90 )
+     {
+      xc = (xc + 32);   
+     }
+    char i = xc;
+    printf("%c", i);
+    }
+
+
+
+
+
+
+
+  }
+    printf("\ncypher rotation value = %d\n", x);
+    
+  
+      
+  
+  
 
     
     
